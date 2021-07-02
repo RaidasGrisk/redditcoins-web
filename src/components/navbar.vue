3261d60371c34a3eb808297f7f6d621a811f2a16
RaidasGrisk <mrraidas@gmail.com>
2021-07-02 20:35:00 +0300

    add ALGO donate address

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -31,9 +31,10 @@
       <div class="card">
         <div class="card-content">
           <div class="content is-size-7-mobile">
-            <b>ETH address:</b><br> 0x864a0Baeb1cA7A45ADbF48700a4C96a8B8F03aB2 <br><br>
-            <b>BTC address:</b><br> 1NLZU3UfYbC4hT4ayJc9n78e679khxFybJ <br><br>
-            <b>NANO address:</b><br> nano_1gzki1hxxfyh7u8gfpbun5i4ga7gperdxuwmqjz498dwkpm6hbxyj69d9xoz
+            <b>ETH:</b><br> 0x864a0Baeb1cA7A45ADbF48700a4C96a8B8F03aB2 <br><br>
+            <b>BTC:</b><br> 1NLZU3UfYbC4hT4ayJc9n78e679khxFybJ <br><br>
+            <b>NANO:</b><br> nano_1gzki1hxxfyh7u8gfpbun5i4ga7gperdxuwmqjz498dwkpm6hbxyj69d9xoz <br><br>
+            <b>ALGO:</b><br> YWQFRGNS2VEYX7G4A7YNBEDRGVE5FMLMFXPEEPPOPIQBYXNZN4FHDNZAFM 
           </div>
         </div>
       </div>
