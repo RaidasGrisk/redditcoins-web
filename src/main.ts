de375b3a63479e24ec1563c6b2417a2eba8b2717
raidas <mrraidas@gmail.com>
2023-06-10 10:48:39 +0300

    add vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,12 @@ import naive from 'naive-ui'
 import store from './store/index'
 import VueApexCharts from 'vue3-apexcharts'
 
+// vercel analytics
+import { inject } from '@vercel/analytics'
+inject()
+
 const app = createApp(App)
+
 
 app.use(naive)
 app.use(store)
